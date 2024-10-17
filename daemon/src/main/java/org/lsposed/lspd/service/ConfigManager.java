cd1ac6de9e8db7374e1a3001e5840720554ce44d
JingMatrix <jingmatrix@gmail.com>
2024-10-17 18:22:48 +0200

    Revert "fix manager not detected"
    This reverts commit 9125b4b2f1439e058fa516a3835edbaa3c561b1c.

--- daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
+++ daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -308,15 +308,14 @@ public class ConfigManager {
 
     public synchronized void updateManager(boolean uninstalled) {
         if (uninstalled) {
-            setAccessMatrixAppId(managerUid, false);
             managerUid = -1;
+            return;
         }
         if (!PackageService.isAlive()) return;
         try {
             PackageInfo info = PackageService.getPackageInfo(BuildConfig.DEFAULT_MANAGER_PACKAGE_NAME, 0, 0);
             if (info != null) {
                 managerUid = info.applicationInfo.uid;
-                setAccessMatrixAppId(managerUid, true);
             } else {
                 managerUid = -1;
                 Log.i(TAG, "manager is not installed");
@@ -533,16 +532,6 @@ public class ConfigManager {
         cachedScope.clear();
     }
 
-    private synchronized void setAccessMatrixAppId(int appId, boolean set) {
-        if (appId < 10000 || appId > 19999) return;
-        int idx = (appId - 10000) >> 3;
-        byte bit = (byte) (1 << ((appId - 10000) & 7));
-        if (set)
-            accessMatrix.put(idx, (byte) (accessMatrix.get(idx) | bit));
-        else
-            accessMatrix.put(idx, (byte) (accessMatrix.get(idx) & ~bit));
-    }
-
     private synchronized void cacheModules() {
         // skip caching when pm is not yet available
         if (!PackageService.isAlive() || !UserService.isAlive()) return;
@@ -750,9 +739,6 @@ public class ConfigManager {
             Log.d(TAG, ps.processName + "/" + ps.uid);
             modules.forEach(module -> Log.d(TAG, "\t" + module.packageName));
         });
-        if (managerUid != -1) {
-            setAccessMatrixAppId(managerUid, true);
-        }
     }
 
     // This is called when a new process created, use the cached result
