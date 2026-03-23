dbf1f4d206d4fd7284eeae89cdbf2987130a6a4e
JingMatrix <jingmatrix@gmail.com>
2026-03-23 09:32:29 +0100

    Fix typo

--- zygisk/src/main/cpp/module.cpp
+++ zygisk/src/main/cpp/module.cpp
@@ -35,7 +35,7 @@ constexpr int PER_USER_RANGE = 100000;
 // Defined via CMake generated marcos
 constexpr uid_t kHostPackageUid = INJECTED_PACKAGE_UID;
 const char *const kHostPackageName = INJECTED_PACKAGE_NAME;
-const char *const kManagePackageName = MANAGER_PACKAGE_NAME;
+const char *const kManagerPackageName = MANAGER_PACKAGE_NAME;
 constexpr uid_t GID_INET = 3003;  // Android's Internet group ID.
 
 enum RuntimeFlags : uint32_t {
@@ -238,7 +238,7 @@ void VectorModule::preAppSpecialize(zygisk::AppSpecializeArgs *args) {
     // grant it internet permissions by adding it to the INET group.
     if (args->uid == kHostPackageUid) {
         lsplant::JUTFString nice_name_str(env_, args->nice_name);
-        if (nice_name_str.get() == std::string(kManagePackageName)) {
+        if (nice_name_str.get() == std::string(kManagerPackageName)) {
             LOGI("Manager app detected. Granting internet permissions.");
             is_manager_app_ = true;
 
@@ -303,7 +303,7 @@ void VectorModule::postAppSpecialize(const zygisk::AppSpecializeArgs *args) {
     }
 
     if (is_manager_app_) {
-        args->nice_name = env_->NewStringUTF(kManagePackageName);
+        args->nice_name = env_->NewStringUTF(kManagerPackageName);
     }
 
     // --- Framework Injection ---
